25f489dd8674012ee15a3a2a006f2f9e8bcbd619
Sumit Srivastava <sumitme00117@gmail.com>
2026-03-21 22:44:56 +0530

    Copy to clipboard

---
+++ src/components/Copy-to-clipboard.tsx
@@ -0,0 +1,32 @@
+'use client';
+import { CopyIcon } from 'lucide-react';
+import React from 'react';
+import { toast } from 'sonner';
+
+const CopyToClipboard = ({
+  textToCopy,
+}: {
+  textToCopy: string;
+  btnLabel?: string;
+}) => {
+  const handleCopyClick = () => {
+    try {
+      navigator.clipboard.writeText(textToCopy);
+      toast.success('Copied to clipboard');
+    } catch (error) {
+      toast.error('Failed to copy to clipboard');
+    }
+  };
+
+  return (
+    <div className="w-full">
+      <button className="w-full" onClick={handleCopyClick}>
+        <div className="flex items-center gap-x-2">
+          Copy <CopyIcon size={15} />
+        </div>
+      </button>
+    </div>
+  );
+};
+
+export default CopyToClipboard;
