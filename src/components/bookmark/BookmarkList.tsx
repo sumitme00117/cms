1e1168649430139a79c50763f410edf1aad0869c
Sumit Srivastava <sumitme00117@gmail.com>
2026-05-02 23:58:25 +0530

    Bookmark List

---
+++ src/components/bookmark/BookmarkList.tsx
@@ -0,0 +1,67 @@
+'use client';
+
+import { TBookmarkWithContent } from '@/actions/bookmark/types';
+import { useRouter } from 'next/navigation';
+import { ContentCard } from '../ContentCard';
+
+const BookmarkList = ({
+  bookmarkData,
+}: {
+  bookmarkData: TBookmarkWithContent[] | { error: string };
+}) => {
+  const router = useRouter();
+  if ('error' in bookmarkData) {
+    return (
+      <h1 className="p-4">Something went wrong, please try again later</h1>
+    );
+  }
+  return (
+    <>
+      <section className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+        {bookmarkData.map((bookmark) => {
+          const {
+            contentId,
+            content: { type, parent, title, hidden, thumbnail, courses },
+          } = bookmark;
+          if ((type === 'video' || type === 'notion') && parent && !hidden) {
+            const { id: folderId, courses } = parent;
+            const courseId = courses[0].courseId;
+            const videoUrl = `/courses/${courseId}/${folderId}/${contentId}`;
+
+            return (
+              <ContentCard
+                type={type}
+                key={contentId}
+                title={title}
+                image={thumbnail || ''}
+                onClick={() => {
+                  router.push(videoUrl);
+                }}
+                bookmark={bookmark}
+                contentId={contentId}
+              />
+            );
+          } else if (type === 'folder' && !parent) {
+            const videoUrl = `/courses/${courses[0].courseId}/${courses[0].contentId}`;
+
+            return (
+              <ContentCard
+                type={type}
+                key={contentId}
+                title={title}
+                image={thumbnail || ''}
+                onClick={() => {
+                  router.push(videoUrl);
+                }}
+                bookmark={bookmark}
+                contentId={contentId}
+              />
+            );
+          }
+        })}
+      </section>
+    </>
+  );
+};
+
+export default BookmarkList;
