3ba4c343fc085de3c943c1c8632388764aaf2920
Sumit Srivastava <sumitme00117@gmail.com>
2026-06-09 23:56:50 +0530

    Courses Page

---
+++ src/app/courses/[courseId]/page.tsx
@@ -0,0 +1,32 @@
+import { QueryParams } from '@/actions/types';
+import { CourseView } from '@/components/CourseView';
+import { getCourse, getFullCourseContent } from '@/db/course';
+import findContentById from '@/lib/find-content-by-id';
+
+export default async function Course({
+  params,
+  searchParams,
+}: {
+  params: { courseId: string };
+  searchParams: QueryParams;
+}) {
+  const courseId = params.courseId;
+  const course = await getCourse(parseInt(courseId, 10));
+  const fullCourseContent = await getFullCourseContent(parseInt(courseId, 10));
+
+  const courseContent = findContentById(fullCourseContent, []);
+
+  const nextContent = null;
+
+  return (
+    <CourseView
+      rest={[]}
+      course={course}
+      nextContent={nextContent}
+      courseContent={courseContent}
+      fullCourseContent={fullCourseContent}
+      searchParams={searchParams}
+      possiblePath=""
+    />
+  );
+}
