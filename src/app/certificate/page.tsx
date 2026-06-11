a7a25d7b4bac4e31ec1809713164daa4ea1db7b8
Sumit Srivastava <sumitme00117@gmail.com>
2026-06-11 22:23:11 +0530

    Certificate

---
+++ src/app/certificate/page.tsx
@@ -0,0 +1,23 @@
+import { CertificateComponent } from '@/components/Certificate';
+import { getCertificates } from '@/db/cert';
+
+const CertificatePage = async () => {
+  const certificates = await getCertificates();
+
+  return (
+    <section className="flex w-full flex-wrap mt-20 items-center justify-center">
+      {certificates?.map(({ cert, course, user }) => (
+        <CertificateComponent
+          certificateId={cert.id}
+          //@ts-ignore
+          course={course}
+          certificateSlug={cert.slug}
+          userName={user.name!}
+          key={course.id}
+        />
+      ))}
+    </section>
+  );
+};
+
+export default CertificatePage;
