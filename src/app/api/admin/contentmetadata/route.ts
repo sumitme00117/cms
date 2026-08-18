f05575f4ba6a2957689fc6fecea030436b536f2c
Sumit Srivastava <sumitme00117@gmail.com>
2026-08-18 23:47:09 +0530

    Content Meta Data Route

---
+++ src/app/api/admin/contentmetadata/route.ts
@@ -0,0 +1,30 @@
+import db from '@/db';
+import { NextRequest, NextResponse } from 'next/server';
+
+export const POST = async (req: NextRequest) => {
+  try {
+    const {
+      updates,
+      contentId,
+      adminPassword,
+    }: {
+      updates: any;
+      contentId: number;
+      adminPassword: string;
+    } = await req.json();
+
+    if (adminPassword !== process.env.ADMIN_SECRET) {
+      return NextResponse.json({}, { status: 403 });
+    }
+    await db.videoMetadata.update({
+      where: {
+        contentId,
+      },
+      data: updates,
+    });
+
+    return NextResponse.json({}, { status: 200 });
+  } catch (err) {
+    return NextResponse.json({}, { status: 500 });
+  }
+};
